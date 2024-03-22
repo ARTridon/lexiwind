e69bad4761280f24e9102ed7ad5a59c9a54687db
ARTridon <artridong@gmail.com>
2024-03-22 14:09:01 +0200

    Update tsconfig.json and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import tsConfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      include: ['src/Editor.tsx'],
+    }),
+    react(),
+    tsConfigPaths(),
+    react(),
+  ],
   build: {
     lib: {
       entry: path.resolve('src', 'Editor.tsx'),
@@ -19,5 +30,4 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
 });
